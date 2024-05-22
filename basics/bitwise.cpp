abf44bf4fdf12ef7d1c0f86eed26c31abac45708
tunashred <enachealexandru.traian@gmail.com>
2024-05-22 23:30:34 +0300

    Tinkered a bit more

--- basics/bitwise.cpp
+++ basics/bitwise.cpp
@@ -2,25 +2,20 @@
 #include <bitset>
 
 int main () {
-    std::bitset<8> mybitset { 0b0000'0101};
+    std::bitset<8> bits{ 0b0011'1010 };
+    std::cout << "bitset size: " << bits.size() << "\n";
+    std::cout << bits.count() << " bits are set to true\n";
+    std::cout << std::boolalpha;
+    std::cout << "are all bits set to true?: " << bits.all() << "\n";
+    std::cout << "are some bits true?: " << bits.any() << "\n";
+    std::cout << "no bits are true?: " << bits.none() << "\n";
 
-    mybitset.set(3);
-    std::cout << "First set: " << mybitset;
-    mybitset.set(3);
-    std::cout << "\nSecond set: " << mybitset;
-
-    mybitset.flip(0);
-    std::cout << "\nFirst flip: " << mybitset;
-    mybitset.flip(0);
-    std::cout << "\nSecond flip: " << mybitset;
-
-    mybitset.reset(3);
-    std::cout << "\nReseted a set bit: " << mybitset;
-    mybitset.reset(0);
-    std::cout << "\nReseted a flipped bit: " << mybitset;
-    mybitset.reset(2);
-    std::cout << "\nReseted a random 1bit: " << mybitset << "\n";
- 
+    std::cout << (bits << 2) << " shifted twice\n";
+    std::cout << ~bits << " logical not\n";
+    std::cout << (std::bitset<4> {0b0101} | std::bitset<4> {0b0110}) << " ORed two bitsets\n";
+    std::cout << (std::bitset<4> {0b0110} & std::bitset<4> {0b0010}) << " ANDed two bitsets\n";
+    std::cout << (std::bitset<4> {0b1100} ^ std::bitset<4> {0b0111}) << " XORed two bitsets\n";
+    
  
     return 0;
 }
