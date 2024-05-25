806d85345e8175e04d21a1135da6b1fd556f8bfc
tunashred <enachealexandru.traian@gmail.com>
2024-05-25 11:37:42 +0300

    Pretty surprising

--- basics/testing.cpp
+++ basics/testing.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
 
-int main() {
-    std::cout << sizeof(long long);
+// assume int is 4 bytes
+int main()
+{
+    signed int s { -1 };
+    unsigned int u { 1 };
+
+    if (s < u) // -1 is implicitly converted to 4294967295, and 4294967295 < 1 is false
+        std::cout << "-1 is less than 1\n";
+    else
+        std::cout << "1 is less than -1\n"; // this statement executes
 
     return 0;
 }
