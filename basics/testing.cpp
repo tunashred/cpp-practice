27752ce93b1bc67dd914e99dc06aae02b8cab7a2
tunashred <enachealexandru.traian@gmail.com>
2024-06-03 17:09:04 +0300

    Interesting fact about int8_t

--- basics/testing.cpp
+++ basics/testing.cpp
@@ -1,15 +1,11 @@
 #include <iostream>
+#include <cstdint>
 
-// assume int is 4 bytes
 int main()
 {
-    signed int s { -1 };
-    unsigned int u { 1 };
+    //std::int8_t and std::uint8_t are actually chars, not integers ;d
+    std::int8_t myInt{65};
+    std::cout << myInt << '\n';
 
-    if (s < u) // -1 is implicitly converted to 4294967295, and 4294967295 < 1 is false
-        std::cout << "-1 is less than 1\n";
-    else
-        std::cout << "1 is less than -1\n"; // this statement executes
-
-    return 0;
+	return 0;
 }
